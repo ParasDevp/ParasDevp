4c0107506bb3022b651b14a7c4af4358d7ff5d80
Paras Juneja <Paras.juneja@nuagebiz.tech>
2022-03-25 00:09:39 +0530

    changes

--- src/Pages/SupplyContract/SupplyContract.js
+++ src/Pages/SupplyContract/SupplyContract.js
@@ -66,132 +66,154 @@ const SupplyContract = () => {
   const [products, setProducts] = useState([]);
   const columns = useMemo(() => tableColumn, []);
   const data = useMemo(() => products, [products]);
-  const[supplierData, setSupplierdata] = useState([]);
-  const[flag,setflag] = useState(false)
+  const [supplierData, setSupplierdata] = useState("");
+  const [flag, setflag] = useState(false);
   const customers = [];
-
-  
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-  useTable(
-    {
-      columns,
-      data,
-    },
-    useSortBy
-    );
-    useEffect(() => {
-      const fetchData = async() =>
-      {
-        const response =  await axios.get('http://localhost:3006/SupplierData')
-        setSupplierdata(response.data);
-      }
-      fetchData()
-      setProducts(customers);
-      setflag(true)
-      if(flag === true)
-      {
-      
-      }
-    }, [flag]);
-    dataa.forEach(
-      ({
-        supplycontractid,
-        supplycontractname,
-        suppliername,
-        contractstartdate,
-        contractenddate,
-        contractstatus,
-        supplycontractlocations,
-      }) => {
-        supplycontractlocations.forEach((customer) => {
-          customers.push({
-            supplycontractid,
-            supplycontractname,
-            suppliername,
-            contractstartdate,
-            contractenddate,
-            contractstatus,
-            ...customer,
+  const fetchData = async () => {
+    const response = await axios
+      .get("http://localhost:3006/SupplierData")
+      .catch((err) => console.log("please start server", err));
+    setSupplierdata(response.data);
+  };
+  useEffect(() => {
+   
+    fetchData();
+    setProducts(customers);
+    if (supplierData) {
+      console.log("we are inside =>", supplierData);
+      supplierData.forEach(
+        ({
+          supplycontractid,
+          supplycontractname,
+          suppliername,
+          contractstartdate,
+          contractenddate,
+          contractstatus,
+          supplycontractlocations,
+        }) => {
+          supplycontractlocations.forEach((customer) => {
+            customers.push({
+              supplycontractid,
+              supplycontractname,
+              suppliername,
+              contractstartdate,
+              contractenddate,
+              contractstatus,
+              ...customer,
+            });
           });
-        });
-      }
-      )
-    const filename = "Supplier Data";
-    // console.log('getting data from axios',supplierData);
-    // console.log('getting data ',dataa);
-    console.log(flag);
-    const [filterPortal, setFiltersPortal] = useState(false);
-    
-    const handlePortal = () => {
-      setFiltersPortal(!filterPortal);
-      console.log(filterPortal);
-    };
-    
-    
-    return (
-    <>
-    {supplierData === '' ? (<> <div style= {{ display : `flex`, justifyContent : `center`, marginTop : `40px`, fontSize : `25px`}}>Loading...</div></>):(<> {filterPortal && <SupplyContractFilter onclose={handlePortal} />}
-      <div style={{ display: `flex`, justifyContent: `space-between` }}>
-        <h1 style={{ margin: `20px`, fontSize: `25px`, fontWeight: `400` }}>
-          Supply Contract
-        </h1>
+        }
+      );
+    }
+  }, [setflag]);
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+      },
+      useSortBy
+    );
 
-        <div style={{ display: `flex` }}>
-      
-          <div className="options">
-            <i className="fa-solid fa-filter" onClick={handlePortal}></i>
-            <div className="dropdown">
-              <button className="dropbtn">
-                <FontAwesomeIcon icon={faDownload} />
-              </button>
-              <div className="dropdown-content">
-                <FontAwesomeIcon icon="fa-solid fa-file-excel" />
-                <ExportToExcel apiData={data} fileName={filename} />
-                <CSVLink filename={filename} data={data}>
-                <FontAwesomeIcon icon={faFileExcel} style = {{ color: `black`, fontSize :`25px`, margin :`10px`}}/>
-                </CSVLink>
+  const filename = "Supplier Data";
+
+  console.log(flag);
+  const [filterPortal, setFiltersPortal] = useState(false);
+
+  const handlePortal = () => {
+    setFiltersPortal(!filterPortal);
+    console.log(filterPortal);
+  };
+  return (
+    <>
+      {supplierData === "" ? (
+        <>
+          {" "}
+          <div
+            style={{
+              display: `flex`,
+              justifyContent: `center`,
+              marginTop: `40px`,
+              fontSize: `25px`,
+            }}
+          >
+            Loading...
+          </div>
+        </>
+      ) : (
+        <>
+          {" "}
+          {filterPortal && <SupplyContractFilter onclose={handlePortal} />}
+          <div style={{ display: `flex`, justifyContent: `space-between` }}>
+            <h1 style={{ margin: `20px`, fontSize: `25px`, fontWeight: `400` }}>
+              Supply Contract
+            </h1>
+
+            <div style={{ display: `flex` }}>
+              <div className="options">
+                <i className="fa-solid fa-filter" onClick={handlePortal}></i>
+                <div className="dropdown">
+                  <button className="dropbtn">
+                    <FontAwesomeIcon icon={faDownload} />
+                  </button>
+                  <div className="dropdown-content">
+                    <FontAwesomeIcon icon="fa-solid fa-file-excel" />
+                    <ExportToExcel apiData={data} fileName={filename} />
+                    <CSVLink filename={filename} data={data}>
+                      <FontAwesomeIcon
+                        icon={faFileExcel}
+                        style={{
+                          color: `black`,
+                          fontSize: `25px`,
+                          margin: `10px`,
+                        }}
+                      />
+                    </CSVLink>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <table colorScheme="white" className="TableMain" {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => (
-            <tr {...headerGroup.getHeaderGroupProps()}>
-              {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render("Header")}
-                  {column.isSorted ? (
-                    column.isSortedDesc ? (
-                      <FontAwesomeIcon icon={faUpLong} />
-                    ) : (
-                      <FontAwesomeIcon icon={faDownLong} />
-                    )
-                  ) : (
-                    ""
-                  )}
-                  {}
-                </th>
+          <table colorScheme="white" className="TableMain" {...getTableProps()}>
+            <thead>
+              {headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                    >
+                      {column.render("Header")}
+                      {column.isSorted ? (
+                        column.isSortedDesc ? (
+                          <FontAwesomeIcon icon={faUpLong} />
+                        ) : (
+                          <FontAwesomeIcon icon={faDownLong} />
+                        )
+                      ) : (
+                        ""
+                      )}
+                      {}
+                    </th>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
-            prepareRow(row);
+            </thead>
+            <tbody {...getTableBodyProps()}>
+              {rows.map((row, i) => {
+                prepareRow(row);
 
-            return (
-              <tr {...row.getRowProps()}>
-                {row.cells.map((cell) => (
-                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
-                ))}
-              </tr>
-            );
-          })}
-        </tbody>
-      </table></>)}
-     
+                return (
+                  <tr {...row.getRowProps()}>
+                    {row.cells.map((cell) => (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      )}
     </>
   );
 };
